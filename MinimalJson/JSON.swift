f97cab8c786ba2d61b0bbd06fc1c60c10bba38f6
Nick Banks <nick.banks@synchromation.com>
2015-01-07 14:53:06 +0000

    WIP - Adding some tests

--- MinimalJson/JSON.swift
+++ MinimalJson/JSON.swift
@@ -12,7 +12,11 @@ import Foundation
 /**
 *  Main struct definition
 */
+
 public class JSON {
+    
+    typealias JSONArray = Array<AnyObject>
+    typealias JSONDictionary = Dictionary<String, AnyObject>
     
     let rootObject: AnyObject
     
@@ -22,7 +26,7 @@ public class JSON {
         if let object: AnyObject = NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments, error: &error) {
             rootObject = object
         } else {
-            rootObject = [:]
+            rootObject = NSNull()
             return nil
         }
     }
@@ -37,35 +41,76 @@ public class JSON {
 *  Subscript support
 */
 
-//extension JSON {
-//    subscript(hash: String) -> JSON {
-//
-//    }
-//}
+extension JSON {
+    
+    subscript(hash: String) -> JSON {
+        if let dictionary = rootObject as? Dictionary<String, AnyObject> {
+            if let object: AnyObject = dictionary[hash] {
+                return JSON(object: object)
+            } else {
+                return JSON(object: NSNull())
+            }
+        } else {
+            return JSON(object: NSNull())
+        }
+    }
+    
+    subscript(index: Int) -> JSON {
+        if let array = rootObject as? Array<AnyObject> {
+            if (index >= 0) && (index < array.count) {
+                return JSON(object: array[index])
+            } else {
+                return JSON(object: NSNull())
+            }
+        } else {
+            return JSON(object: NSNull())
+        }
+    }
+}
 
 /**
 *  Value support
 */
 
 extension JSON {
-    func string() -> String? {
-        return rootObject as? String
+
+    var array: JSONArray? {
+        return rootObject as? JSONArray
     }
     
-    func int() -> Int? {
-        return rootObject as? Int
+    var dictionary: JSONDictionary? {
+        return rootObject as? JSONDictionary
     }
     
-    func bool() -> Bool? {
+    var bool: Bool? {
         if let i = rootObject as? Int {
-            return Bool(i)
+            if (i == 0) || (i == 1) {
+                return Bool(i)
+            } else {
+                return nil
+            }
         } else {
             return nil
         }
     }
     
-    //    func array() -> Array? {
-    //        return rootObject as? Array
-    //    }
+    var string: String? {
+        return rootObject as? String
+    }
     
+    var int: Int? {
+        return rootObject as? Int
+    }
+
+    var unsignedInt: UInt? {
+        return rootObject as? UInt
+    }
+    
+    var float: Float? {
+        return rootObject as? Float
+    }
+    
+    var double: Double? {
+        return rootObject as? Double
+    }
 }
--- MinimalJsonTests/MinimalJsonTests.swift
+++ MinimalJsonTests/MinimalJsonTests.swift
@@ -11,31 +11,91 @@ import XCTest
 
 class MinimalJsonTests: XCTestCase {
     
-    var JSONData: NSData!
+    let defaultString = "Default"
+    let defaultIntZero = 0
+    let defaultInt = 9999
+    let defaultDoubleZero = 0.0
+    let defaultDouble = 9999.0
+
+
+    var parsedJSON: JSON!
     
     override func setUp() {
         super.setUp()
-
-        if let file = NSBundle(forClass: MinimalJsonTests.self).pathForResource("TwitterUserTimeline", ofType: "json") {
-            JSONData = NSData(contentsOfFile: file)
+        
+        if let file = NSBundle(forClass: MinimalJsonTests.self).pathForResource("Test", ofType: "json") {
+            if let data = NSData(contentsOfFile: file) {
+                if let parsedData = JSON(data: data) {
+                    self.parsedJSON = parsedData
+                } else {
+                    XCTFail("Failed to parse JSON from file")
+                }
+            } else {
+                XCTFail("Failed to create NSData from JSON")
+            }
         } else {
-            XCTFail("Missing TwitterUserTimeline file")
+            XCTFail("Missing JSON file")
         }
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testBoolean() {
+        var b1 = parsedJSON["missingkey"].bool
+        XCTAssertNil (b1, "Doesn't return nil for missing key")
+        
+        var b2 = parsedJSON["booltrue"].bool ?? false
+        XCTAssertTrue (b2, "Doesn't return true")
+        
+        var b3 = parsedJSON["boolfalse"].bool ?? true
+        XCTAssertFalse (b3, "Doesn't return false")
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
+    func testString() {
+        var s1 = parsedJSON["missingkey"].string ?? defaultString
+        XCTAssertEqual (s1, defaultString, "Doesn't return nil for missing key")
         
-        var parsedJSON = JSON(data: JSONData)
+        var s2 = parsedJSON["string"].string ?? defaultString
+        XCTAssertEqual (s2, "test", "Doesn't return string")
         
-        XCTAssertNotNil(parsedJSON, "Pass")
+        var s3 = parsedJSON["stringempty"].string ?? defaultString
+        XCTAssertEqual (s3, "", "Doesn't return empty string")
     }
     
+    func testInt() {
+        var i1 = parsedJSON["missingkey"].int
+        XCTAssertNil (i1, "Doesn't return nil for missing key")
+        
+        var i2 = parsedJSON["intzero"].int ?? defaultInt
+        XCTAssertEqual (i2, defaultIntZero, "Doesn't return 0")
+        
+        var i3 = parsedJSON["intpositive"].int ?? defaultIntZero
+        XCTAssertEqual (i3, defaultInt, "Doesn't return int value")
+    }
+    
+    func testUnsignedInt() {
+        var ui1 = parsedJSON["missingkey"].unsignedInt
+        XCTAssertNil (ui1, "Doesn't return nil for missing key")
+        
+        var ui2 = parsedJSON["intzero"].unsignedInt ?? UInt(defaultInt)
+        XCTAssertEqual (ui2, UInt(defaultIntZero), "Doesn't return 0")
+        
+        var ui3 = parsedJSON["intpositive"].unsignedInt ?? UInt(defaultIntZero)
+        XCTAssertEqual (ui3, UInt(defaultInt), "Doesn't return int value")
+        
+        var ui4 = parsedJSON["intpositive"].unsignedInt
+        XCTAssertNil (ui4, "Doesn't return nil for negative integer")
+    }
+    
+    func testFloat() {
+        var f1 = parsedJSON["missingkey"].float
+        XCTAssertNil (f1, "Doesn't return nil for missing key")
+        
+        var f2 = parsedJSON["intzero"].float ?? Float(defaultDouble)
+        XCTAssertEqual (f2, Float(defaultDoubleZero), "Doesn't return 0.0")
+        
+        var f3 = parsedJSON["intpositive"].int ?? defaultIntZero
+        XCTAssertEqual (f3, defaultInt, "Doesn't return int value")
+    }
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock() {
